5017f9207b43c88501f0aa41c953f5d3b4f3f687
Mark Sommers <mssommer@ncsu.edu>
2021-02-17 11:49:34 -0500

    Matched Grade Tracker Deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URI
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "2.4.0"
-	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.4.10"
-	kotlin("plugin.spring") version "1.4.10"
+	id("org.springframework.boot") version "2.4.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.30"
+	kotlin("plugin.spring") version "1.4.30-RC"
 }
 
 group = "com.marks0mmers"
@@ -14,26 +14,28 @@ java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
-	maven {
-		url = URI.create("https://repo.maven.apache.org/maven2")
-	}
 }
 
 dependencies {
 	implementation("org.springframework.boot", "spring-boot-starter-data-mongodb-reactive")
 	implementation("org.springframework.boot", "spring-boot-starter-security")
+	implementation("org.springframework.boot", "spring-boot-starter-validation")
 	implementation("org.springframework.boot", "spring-boot-starter-webflux")
 	implementation("io.jsonwebtoken", "jjwt", "0.9.1")
 	implementation("com.fasterxml.jackson.module", "jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin", "reactor-kotlin-extensions")
+	implementation("jakarta.xml.bind", "jakarta.xml.bind-api")
 	implementation("org.jetbrains.kotlin", "kotlin-reflect")
 	implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-reactor")
 	implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core")
-	implementation("javax.xml.bind", "jaxb-api")
 	testImplementation("org.springframework.boot", "spring-boot-starter-test")
 	testImplementation("io.projectreactor", "reactor-test")
 	testImplementation("org.springframework.security", "spring-security-test")
+}
+
+tasks.withType<BootJar> {
+	archiveFileName.set("${archiveBaseName.get()}.${archiveExtension.get()}")
 }
 
 tasks.withType<KotlinCompile> {
