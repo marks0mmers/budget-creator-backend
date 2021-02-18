a5403247defb4923fcab0deb8e43593df0cc632b
Mark Sommers <mssommer@ncsu.edu>
2021-02-18 16:40:04 -0500

    Removed SecurityContextRepository.kt

--- src/main/kotlin/com/marks0mmers/budgetcreator/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/marks0mmers/budgetcreator/config/WebSecurityConfig.kt
@@ -1,37 +1,41 @@
 package com.marks0mmers.budgetcreator.config
 
-import com.marks0mmers.budgetcreator.repositories.SecurityContextRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import reactor.core.publisher.Mono
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 class WebSecurityConfig {
-
     @Autowired lateinit var authenticationManager: AuthenticationManager
-    @Autowired lateinit var securityContextRepository: SecurityContextRepository
+    @Autowired lateinit var authenticationConverter: JwtServerAuthenticationConverter
 
     @Bean
-    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain = http
-        .csrf().disable()
-        .formLogin().disable()
-        .httpBasic().disable()
-        .authenticationManager(authenticationManager)
-        .securityContextRepository(securityContextRepository)
-        .exceptionHandling()
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        val authenticationWebFilter = AuthenticationWebFilter(authenticationManager).apply {
+            setServerAuthenticationConverter(authenticationConverter)
+        }
+        return http.authorizeExchange()
+            .pathMatchers(HttpMethod.OPTIONS).permitAll()
+            .pathMatchers(HttpMethod.POST,"/api/users", "/api/users/login").permitAll()
+            .anyExchange().authenticated()
+            .and()
+            .exceptionHandling()
             .authenticationEntryPoint { swe, _ -> Mono.fromRunnable { swe.response.statusCode = HttpStatus.UNAUTHORIZED } }
             .accessDeniedHandler { swe, _ -> Mono.fromRunnable { swe.response.statusCode = HttpStatus.FORBIDDEN } }
-        .and()
-        .authorizeExchange()
-            .pathMatchers(HttpMethod.OPTIONS).permitAll()
-            .pathMatchers(HttpMethod.POST, "/api/users", "/api/users/login").permitAll()
-            .anyExchange().authenticated()
-        .and().build()
+            .and()
+            .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .build()
+    }
 }
